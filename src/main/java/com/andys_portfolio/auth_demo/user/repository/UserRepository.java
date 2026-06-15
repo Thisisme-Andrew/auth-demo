432fc3201b3ea093211e0cc4c258875e814d2ce5
Thisisme-Andrew <thisemailstotroll@gmail.com>
2026-06-15 13:21:05 +0700

    user respository created

---
+++ src/main/java/com/andys_portfolio/auth_demo/user/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.andys_portfolio.auth_demo.user.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.andys_portfolio.auth_demo.user.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  Optional<User> findByEmail(String email);
+
+  boolean existsByEmail(String email);
+}
